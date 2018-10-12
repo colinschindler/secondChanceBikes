6b81e2f457c042ba3c1390833918ae6282b45389
Colin Schindler <schindlercolin@gmail.com>
2018-10-12 19:42:41 -0400

    working on fixed shrinking navbar

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function stickyNav() {
+  const nav = document.querySelector('#nav-bar');
+  const logo = document.querySelector('#nav-logo');
+  const logoImg = document.querySelector('#logo');
+  const navTop = nav.offsetTop;
+
+  if (window.scrollY >= navTop) {
+    nav.classList.add('sticky-nav');
+    logo.classList.add('corner-logo');
+    logoImg.src = 'images/home/logo-small.png';
+  } else {
+    nav.classList.remove('sticky-nav');
+    logo.classList.remove('corner-logo');
+    logoImg.src = 'images/home/logo-big.png';
+  }
+}
+
+window.addEventListener('scroll', stickyNav);
