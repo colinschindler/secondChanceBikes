b746f5d000f25d44e2e39ad655b56e61cf1fb1ea
Colin Schindler <schindlercolin@gmail.com>
2018-10-28 19:51:52 -0400

    added a js fade to navbar

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,6 @@ function stickyNav() {
   const logoImg = document.querySelector('#logo');
   const navTop = nav.offsetTop;
 
-  console.log ('navTop = ' + navTop);
-  console.log ('scrollY = ' + scrollY);
-
   if (window.scrollY > navTop) {
     nav.classList.add('sticky-nav');
     logo.classList.add('corner-logo');
@@ -19,3 +16,27 @@ function stickyNav() {
 }
 
 window.addEventListener('scroll', stickyNav);
+
+var isScrolling;
+
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+
+  const nav = document.querySelector('#nav-bar');
+
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+
+    // Run the callback
+    if (scrollY > 1) {
+      nav.classList.add('fade');
+    }
+
+  }, 2000);
+  
+  nav.classList.remove('fade');
+
+}, false);
